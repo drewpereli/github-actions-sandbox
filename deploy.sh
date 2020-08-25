c3483a2747f0505ed39c583aa3f6ede5e422667a
Drew Pereli <drewpereli@gmail.com>
2020-08-25 15:28:52 -0700

    foo

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-echo "abc"
-echo "def"
-# cwd=$(pwd)
-# function_dirs=("./lambda-functions/main/" "./lambda-functions/screenshot/")
-# function_names=($MAIN_FUNCTION_NAME $SCREENSHOT_FUNCTION_NAME)
 
-# for ((idx=0; idx<${#function_dirs[@]}; ++idx)); do
-#   cd ${function_dirs[idx]}
-#   function_name="${function_names[idx]}"
-#   zip_file_name="$function_name.zip"
+cwd=$(pwd);
+function_dirs=("./lambda-functions/main/" "./lambda-functions/screenshot/");
+function_names=($MAIN_FUNCTION_NAME $SCREENSHOT_FUNCTION_NAME);
 
-#   mkdir -p ./node_modules
-#   npm install --production --silent
+for ((idx=0; idx<${#function_dirs[@]}; ++idx)); do
+  cd ${function_dirs[idx]}
+  function_name="${function_names[idx]}"
+  zip_file_name="$function_name.zip"
 
-#   zip -r -q $zip_file_name .
+  mkdir -p ./node_modules
+  npm install --production --silent
 
-#   aws s3 cp $zip_file_name s3://$DEPLOYMENT_S3_BUCKET/
-#   aws lambda update-function-code --function-name=$function_name --s3-bucket=$DEPLOYMENT_S3_BUCKET --s3-key=$zip_file_name --region=$AWS_REGION
-#   cd ${cwd}
-# done
+  zip -r -q $zip_file_name .
+
+  aws s3 cp $zip_file_name s3://$DEPLOYMENT_S3_BUCKET/
+  aws lambda update-function-code --function-name=$function_name --s3-bucket=$DEPLOYMENT_S3_BUCKET --s3-key=$zip_file_name --region=$AWS_REGION
+  cd ${cwd}
+done
